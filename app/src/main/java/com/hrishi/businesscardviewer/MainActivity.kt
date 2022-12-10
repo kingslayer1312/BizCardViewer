18af8e18b739ff8738a99fbf23c22f900b2474f0
Kingslayer1302 <hrishabari@gmail.com>
2022-12-10 10:22:24 +0530

    first commit

---
+++ app/src/main/java/com/hrishi/businesscardviewer/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.hrishi.businesscardviewer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hrishi.businesscardviewer.ui.theme.BusinessCardViewerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BusinessCardViewerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BusinessCardViewerTheme {
+        Greeting("Android")
+    }
+}
