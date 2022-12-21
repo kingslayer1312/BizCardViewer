7031d5af9e538ed61d85840bd58fe978c327e722
Kingslayer1302 <hrishabari@gmail.com>
2022-12-21 21:11:44 +0530

    Added some functionality to Edit Portfolio button (still under progress)

--- app/src/main/java/com/hrishi/businesscardviewer/MainActivity.kt
+++ app/src/main/java/com/hrishi/businesscardviewer/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.hrishi.businesscardviewer
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -9,36 +8,23 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.materialIcon
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hrishi.businesscardviewer.ui.theme.BusinessCardViewerTheme
 
-val ChampagnePink = Color(0xFFEFD9CE)
-val Plum = Color(0xFF8F3985)
-val SpaceCadet = Color(0xFF25283D)
-val BlizzardBlue = Color(0xFF98DFEA)
 val IndigoDye = Color(0xFF284B63)
 val Gainsboro = Color(0xFFD9D9D9)
 val Ming = Color(0xFF3C6E71)
@@ -75,7 +61,7 @@ fun CreateBizCard() {
     }
 
 
-    androidx.compose.material.Surface(
+    Surface(
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight(),
@@ -116,7 +102,6 @@ fun CreateBizCard() {
                         shape = RoundedCornerShape(50),
                         onClick = {
                             buttonClickedState1.value = !buttonClickedState1.value
-
                         }) {
                         Text(
                             text = "EDIT DETAILS",
@@ -169,7 +154,23 @@ fun CreateBizCard() {
 
 @Composable
 fun EditContent() {
-    /*TODO*/
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(10),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+            PortfolioEdit()
+        }
+    }
 }
 
 @Composable
@@ -188,15 +189,150 @@ fun Content() {
             shape = RoundedCornerShape(10),
             border = BorderStroke(width = 2.dp, color = Color.LightGray)
         ) {
-            Portfolio(data = titles)
-
-
+            Portfolio()
         }
     }
 }
 
 @Composable
-fun Portfolio(data: List<String>) {
+fun PortfolioEdit() {
+    LazyColumn(modifier = Modifier.background(Ming)) {
+        item {
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(50),
+                elevation = 4.dp,
+                backgroundColor = Gainsboro
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(Gainsboro)
+                        .padding(7.dp)
+                ) {
+                    CreatePhoneImage(modifier = Modifier.size(100.dp))
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = Alignment.CenterVertically)
+                    ) {
+                        Text(text = titles[0], color = Color.Black, fontWeight = FontWeight.Bold)
+                        PhoneTextBox()
+                    }
+
+                }
+            }
+        }
+
+        item {
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(50),
+                elevation = 4.dp,
+                backgroundColor = Gainsboro
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(Gainsboro)
+                        .padding(7.dp)
+                ) {
+                    CreateMailImage(modifier = Modifier.size(100.dp))
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = Alignment.CenterVertically)
+                    ) {
+                        Text(text = titles[1], color = Color.Black, fontWeight = FontWeight.Bold)
+                        EmailTextBox()
+                    }
+
+                }
+            }
+        }
+
+        item {
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(50),
+                elevation = 4.dp,
+                backgroundColor = Gainsboro
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(Gainsboro)
+                        .padding(7.dp)
+                ) {
+                    CreateLinkedInImage(modifier = Modifier.size(100.dp))
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = Alignment.CenterVertically)
+                    ) {
+                        Text(text = titles[2], color = Color.Black, fontWeight = FontWeight.Bold)
+                        LinkedinTextBox()
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PhoneTextBox() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    OutlinedTextField(
+        modifier = Modifier.background(Gainsboro),
+        value = text,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Gainsboro,
+            unfocusedBorderColor = Color.DarkGray),
+        onValueChange = { newText ->
+            text = newText
+        }
+    )
+}
+
+@Composable
+fun EmailTextBox() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    OutlinedTextField(
+        modifier = Modifier.background(Gainsboro),
+        value = text,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Gainsboro,
+            unfocusedBorderColor = Color.DarkGray),
+        onValueChange = { newText ->
+            text = newText
+        }
+    )
+}
+
+@Composable
+fun LinkedinTextBox() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    OutlinedTextField(
+        modifier = Modifier.background(Gainsboro),
+        value = text,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Gainsboro,
+            unfocusedBorderColor = Color.DarkGray),
+        onValueChange = { newText ->
+            text = newText
+        }
+    )
+}
+
+@Composable
+fun Portfolio() {
     LazyColumn(modifier = Modifier.background(Ming)) {
         item {
             Card(
@@ -285,8 +421,6 @@ fun Portfolio(data: List<String>) {
             }
         }
     }
-
-
 }
 
 @Composable
@@ -391,13 +525,3 @@ private fun CreateLinkedInImage(modifier: Modifier = Modifier) {
         )
     }
 }
-
-
-//@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BusinessCardViewerTheme {
-
-    }
-}
-
